ef8d7ad9bfe32333e2a21c63d466bbf9965b9c77
ManojKasun <manojkasun1996@gmail.com>
2022-04-09 14:07:38 +0530

    add initial project

---
+++ src/test/java/com/blood_bank_api/blood_bank_api/BloodBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blood_bank_api.blood_bank_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
